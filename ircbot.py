9e15556cd54da5c923134acf6f53c5ee895c92d9
Mark Winter <wintermarkedward@gmail.com>
2015-07-05 22:22:39 +0100

    Implement command system

--- ircbot.py
+++ ircbot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 from linkresolver import LinkResolver
+from watcher import Watcher
 from re import findall
 from circuits import Debugger
 from circuits import Component
@@ -9,6 +10,33 @@ from circuits.protocols.irc import IRC, PRIVMSG, USER, NICK, JOIN
 from circuits.protocols.irc import ERR_NICKNAMEINUSE
 from circuits.protocols.irc import RPL_ENDOFMOTD, ERR_NOMOTD
 
+class Source():
+    def execute(self, keywords, target, source, ircbot):
+        ircbot.fire(PRIVMSG(target, "Source available here: https://github.com/Astonex/IRC-Bot/"))
+
+    def usage(self):
+        pass
+
+class Join():
+    def execute(self, keywords, target, source, ircbot):
+        if len(keywords) != 2:
+            return -1
+
+        if source[0] == "astonex" or source[0] == "softkitty":
+            ircbot.fire(JOIN(keywords[1]))
+        else:
+            ircbot.fire(PRIVMSG(target, "Must be authorised to use this command"))
+
+    def usage(self):
+        return "Usage: !join <channel name>"
+
+class Commands():
+    def execute(self, keywords, target, source, ircbot):
+        ircbot.fire(PRIVMSG(target, ircbot.list_commands()))
+
+    def usage(self):
+        pass
+
 class Bot(Component):
     def init(self, host="irc.freenode.net", port="6667", channel="softkitty"):
         self.host = host
@@ -16,11 +44,23 @@ class Bot(Component):
         self.nick = "fluffybot"
         self.password = open("password").read().strip()
 
+        self.command_char = "!"
+
+        self.linkresolver = LinkResolver()
+
+        self.commands = {
+            'commands': Commands(),
+            'help': Commands(),
+            'join': Join(),
+            'source': Source(),
+            'watch': Watcher()
+        }
+
         TCPClient(channel=self.channel).register(self)
         IRC(channel=self.channel).register(self)
 
     def ready(self, component):
-        """ Ready Event
+        """ Ready Event.
 
         Triggered by the underlying tcpclient Component when it is ready to
         start making a new connection.
@@ -29,7 +69,7 @@ class Bot(Component):
         self.fire(connect(self.host, self.port))
 
     def connected(self, host, port):
-        """Connected Event
+        """Connected Event.
 
         Triggered by the underlying tcpclient Component when a successfully
         connection has been made.
@@ -39,7 +79,7 @@ class Bot(Component):
         self.fire(USER(self.nick, self.nick, host, "Link Resolver Bot"))
 
     def disconnected(self):
-        """Disconnected Event
+        """Disconnected Event.
 
         Triggered by the underlying tcpclient Component when the connection is lost
         """
@@ -47,14 +87,13 @@ class Bot(Component):
         raise SystemExit(0)
 
     def numeric(self, source, numeric, *args):
-        """Numeric Event
+        """Numeric Event.
 
         Triggered by the irc Protocol Component when we have received an irc
         Numberic Event from server we are connected to
         """
 
         # if nick is in use add an _ to the end of our name
-        # although this kind of fucks up our nickserv identify
         if numeric == ERR_NICKNAMEINUSE:
             self.fire(NICK("{0:s}_".format(args[0])))
 
@@ -63,25 +102,40 @@ class Bot(Component):
             self.fire(PRIVMSG("NICKSERV", "IDENTIFY " + self.password))
 
     def privmsg(self, source, target, message):
-        """Message Event
+        """Message Event.
 
         Triggered by the irc Protocol Component for each message we receieve
         from the server
         """
 
-        if target[0] == "#":
-            urls = findall("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", message)
-
-            if urls:
-                title = LinkResolver().get_title(urls[0])
-                self.fire(PRIVMSG(target, title))
+        if message[:1] == self.command_char:
+            self.parse(message, source, target)
 
         else:
-            if source[0] == "softkitty" or source[0] == "astonex":
-                self.fire(JOIN(message))
-            else:
-                self.fire(PRIVMSG(source[0], message))
+            urls = findall("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", message)
+            if urls:
+                title = self.linkresolver.get_title(urls[0])
+                self.fire(PRIVMSG(target, title))
 
+    def parse(self, message, source, target):
+        keywords = message.split(' ')
+
+        for key in self.commands:
+            if key == keywords[0][1:]:
+                response = self.commands[key].execute(keywords, target, source, self)
+
+                if response == -1:
+                    self.fire(PRIVMSG(target, self.commands[key].usage()))
+
+                break
+
+    def list_commands(self):
+        list = ""
+
+        for key in self.commands:
+            list += self.command_char + str(key) + " "
+
+        return "Available commands: " + list
 
 bot = Bot(*sys.argv[1:])
 Debugger().register(bot)
---
+++ watcher.py
@@ -0,0 +1,10 @@
+class Watcher():
+    def __init__(self):
+        pass
+
+    def execute(self, keywords, target, source, ircbot):
+        return -1
+
+    def usage(self):
+        return "Not yet implemented"
+        #return "Usage: !watch <board id> <keyword>"
