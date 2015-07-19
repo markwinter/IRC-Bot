6d156cd51358efdb144d133135b3446f640eb91d
Mark Winter <wintermarkedward@gmail.com>
2015-07-19 18:55:29 +0100

    Save quotes to file and fix getting random quote

--- quote.py
+++ quote.py
@@ -2,12 +2,18 @@ from threading import Thread
 from circuits.protocols.irc import PRIVMSG
 import random
 import time
+from pickle import load, dump
 
 class Quote(Thread):
     def __init__(self):
         Thread.__init__(self)
 
-        self.quotes = []
+        # Load previously saved quotes if possible
+        try:
+            with open('saved_quotes') as f:
+                self.quotes = load(f)
+        except:
+            self.quotes = []
 
         self.running = False
         self.setDaemon(True)
@@ -18,7 +24,7 @@ class Quote(Thread):
             return -1
 
         if keywords[1] == "add":
-            # Check they gave a quote
+            # Check they gave at least a one word quote
             if len(keywords) < 3:
                 return -1
 
@@ -30,8 +36,11 @@ class Quote(Thread):
             quote_date = time.strftime("%d/%m/%Y")
 
             quote = "" + quote_date + " " + quote_time + " || " + quote
-
             self.quotes.append(quote)
+
+            with open('saved_quotes', 'w') as f:
+                dump(self.quotes, f) # do we need to save entire structure each time?
+
             ircbot.fire(PRIVMSG(target, "Added quote #" + str(len(self.quotes) - 1)))
 
         elif keywords[1] == "del":
@@ -51,25 +60,27 @@ class Quote(Thread):
             if len(self.quotes) == 0:
                 ircbot.fire(PRIVMSG(target, "No quotes in system"))
                 return
-            elif len(self.quotes) == 1:
+            elif len(self.quotes) == 1: # randrange wouldnt work with randrange(0)
                 quote = self.quotes[0]
                 quote = "#0 " + quote
                 ircbot.fire(PRIVMSG(target, quote))
             else:
-                num = random.randrange(len(self.quotes) - 1)
-                quote = self.quotes[num]
+                num = random.randrange(len(self.quotes))
+                quote = "#" + str(num) + " " + self.quotes[num]
                 ircbot.fire(PRIVMSG(target, quote))
 
         elif keywords[1] == "get":
             if len(keywords) != 3:
                 return -1
 
-            if self.quotes[int(keywords[2])]:
-                quote = self.quotes[int(keywords[2])]
-                quote = "#" + str(int(keywords[2])) + " " + quote
-                ircbot.fire(PRIVMSG(target, quote))
-            else:
+            quote_id = int(keywords[2])
+
+            if quote_id < 0 or quote_id >= len(self.quotes):
                 ircbot.fire(PRIVMSG(target, "Invalid quote id"))
+            else:
+                quote = self.quotes[quote_id]
+                quote = "#" + str(quote_id) + " " + quote
+                ircbot.fire(PRIVMSG(target, quote))
 
         else:
             return -1
